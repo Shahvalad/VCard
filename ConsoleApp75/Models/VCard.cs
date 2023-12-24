40772294b7a7c4949da8f5e5205eb4185a206578
Shahvalad <shahvaladayvazov@gmail.com>
2023-12-24 19:40:14 +0400

    initial commit

---
+++ ConsoleApp75/Models/VCard.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp75.Models
+{
+    public class VCard
+    {
+        public string Id { get; set; }
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+
+
+        public string ToVCard()
+        {
+            return
+                "BEGIN:VCARD" + 
+                $"FN:{Firstname} {Surname}" + Environment.NewLine +
+                $"N:{Surname};{Firstname};;;" + Environment.NewLine +
+                $"TEL:{Phone}" + Environment.NewLine +
+                $"EMAIL:{Email} " + Environment.NewLine +
+                $"ADR:;;{City};;{Country};;" + Environment.NewLine +
+                "END:VCARD";
+        }
+    }
+
+
+}
---
+++ ConsoleApp75/Program.cs
@@ -0,0 +1,50 @@
+﻿using ConsoleApp75.Models;
+using Newtonsoft.Json.Linq;
+using System.Text;
+using System.Text.Json;
+
+List<VCard> vcards = new();
+string filePath = "C:\\Users\\Шахвалад\\source\\repos\\ConsoleApp75\\ConsoleApp75\\vcards.json";
+
+using (HttpClient client = new HttpClient())
+{
+    string url = "https://randomuser.me/api?results=50&authuser=0";
+    var result = await client.GetAsync(url);
+
+    if (result.IsSuccessStatusCode)
+    {
+        string json = await result.Content.ReadAsStringAsync();
+
+        JObject data = JObject.Parse(json);
+        JArray resultsArray = (JArray)data["results"];
+        foreach (var resultItem in resultsArray)
+        {
+            vcards.Add(new VCard
+            {
+                Id = resultItem["id"]["value"].ToString(),
+                Firstname = resultItem["name"]["first"].ToString(),
+                Surname = resultItem["name"]["last"].ToString(),
+                Email = resultItem["email"].ToString(),
+                Phone = resultItem["phone"].ToString(),
+                Country = resultItem["location"]["country"].ToString(),
+                City = resultItem["location"]["city"].ToString()
+            });
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Error: {result.StatusCode}");
+    }
+}
+
+foreach (var vcard in vcards)
+{
+    var vcardString = vcard.ToVCard();
+    WriteToJson<string>(vcardString, filePath);
+}
+
+void WriteToJson<T>(T data, string filePath)
+{
+    string json = JsonSerializer.Serialize(data);
+    File.AppendAllText(filePath, json + Environment.NewLine);
+}
