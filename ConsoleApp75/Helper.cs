73affd56fdc3632e1231b539592242aa1dfbd12b
Shahvalad <89097428+Shahvalad@users.noreply.github.com>
2024-01-08 08:59:09 +0400

    Add files via upload

---
+++ ConsoleApp75/Helper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp75
+{
+    internal static class Helper
+    {
+        public static bool isExisting(string filepath)
+        {
+            return File.Exists(filepath);
+        }
+    }
+}
